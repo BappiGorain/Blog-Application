9c0a5c328592b49ddcfae7bea9a3ce01263f3fe4
BappiGorain <bappigorain08312@gmail.com>
2024-09-12 23:05:22 +0530

    new files added

--- blog-app-apis/src/main/java/com/codewithbappi/blog/impl/PostServiceImpl.java
+++ blog-app-apis/src/main/java/com/codewithbappi/blog/impl/PostServiceImpl.java
@@ -1,6 +1,51 @@
 package com.codewithbappi.blog.impl;
 
-public class PostServiceImpl
+import com.codewithbappi.blog.entities.Post;
+import com.codewithbappi.blog.payloads.PostDto;
+import com.codewithbappi.blog.services.PostService;
+
+import java.util.List;
+
+public class PostServiceImpl implements PostService
 {
 
+    @Override
+    public Post createPost(PostDto postDto) {
+        return null;
+    }
+
+    @Override
+    public Post updatePost(PostDto postDto, Integer postId) {
+        return null;
+    }
+
+    @Override
+    public void deletePost(Integer postId) {
+
+    }
+
+    @Override
+    public List<Post> getAllPost() {
+        return List.of();
+    }
+
+    @Override
+    public Post getPostById(Integer postId) {
+        return null;
+    }
+
+    @Override
+    public List<Post> getPostsByCategory(Integer categoryId) {
+        return List.of();
+    }
+
+    @Override
+    public List<Post> getPostsByUser(Integer userId) {
+        return List.of();
+    }
+
+    @Override
+    public List<Post> searchPosts(String keyword) {
+        return List.of();
+    }
 }
